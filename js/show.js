75bf0170ab1926c74d1f455e978e4cedb25a653c
Dip Ghosh <dip.ghosh@shohoz.com>
2023-01-18 11:39:52 +0600

    design changes

--- js/show.js
+++ js/show.js
@@ -8,23 +8,23 @@ const search = document.getElementById("search");
 
 const getWeatherByLocation = async (city) => {
 
-    const apiUrl = url + `?q=${city}&appid=${apikey}`;
-
-    const response = await fetch(apiUrl);
+    const apiUrl       = url + `?q=${city}&appid=${apikey}`;
+    const response     = await fetch(apiUrl);
     const responseData = await response.json();
 
-    console.log(responseData);
     addWeatherToPage(responseData);
 }
 
 const convertKelvinToCelsius = (temp) => {
+
     return Math.floor(temp - 273.15);
 }
 
 function addWeatherToPage(data) {
-    const temp = convertKelvinToCelsius(data.main.temp);
+
+    const temp        = convertKelvinToCelsius(data.main.temp);
     const feelingTemp = convertKelvinToCelsius(data.main.feels_like);
-    const weather = document.createElement("div");
+    const weather     = document.createElement("div");
 
     weather.classList.add("weather");
 
@@ -33,23 +33,20 @@ function addWeatherToPage(data) {
         <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
         <small>${data.weather[0].main}</small>
         <p>Real Feel - <span style="font-size: 40px;font-weight: bold "> ${feelingTemp}°C </span> </p>
-        <p>Humidity - <span style="font-size: 20px;font-weight: bold "> ${data.main.humidity}</span> </p>
+        <p>Humidity - <span style="font-size: 40px;font-weight: bold "> ${data.main.humidity}</span> </p>
     `;
 
     main.innerHTML = "";
-
     main.appendChild(weather);
 }
 
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+form.addEventListener("submit", (event) => {
 
+    event.preventDefault();
     const city = search.value;
 
-    if (city) {
-        getWeatherByLocation(city);
-    }
+    if (city) getWeatherByLocation(city);
 });
 
-export { getWeatherByLocation}
+export { getWeatherByLocation }
